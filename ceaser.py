efb0afc345cbdf3c30d950e6f7f8222d24d11a8e
ericq12 <zbruins33@gmail.com>
2018-08-21 13:54:08 -0400

    Cleanup
    Cleaned up code

--- ceaser.py
+++ ceaser.py
@@ -1,18 +1,18 @@
 import string
 import collections
 mode = input("press e for encryption or d for decryption\n")
-if(mode == "e"):
+plaintext = input("enter your message\n")
+shift = int(input("enter the shift\n"))
+
+def encrypt():
     #encrypt
-    plaintext = input("enter your plaintext\n")
     x = len(plaintext)
-    shift = int(input("enter the shift\n"))
     plainkey = collections.deque(string.ascii_lowercase)
     plainkey.rotate(-shift)
     cipherkey = list(plainkey)
     plainkey.rotate(shift)
     plainkey.append(" ")
     cipherkey.append(" ")
-    print(cipherkey)
     i = 0
     while (i < x):
         y = 0
@@ -24,11 +24,11 @@ if(mode == "e"):
         i = i + 1
     print(" ")
 
-if(mode == "d"):
+def decrypt():
     #decrypt
-    plaintext = input("enter the ciphertext\n")
+    # plaintext = input("enter the ciphertext\n")
     x = len(plaintext)
-    shift = int(input("enter the shift\n"))
+    # shift = int(input("enter the shift\n"))
     plainkey = collections.deque(string.ascii_lowercase)
     plainkey.rotate(shift)
     cipherkey = list(plainkey)
@@ -45,3 +45,9 @@ if(mode == "d"):
             print(encryption, end='')
         i = i + 1
     print(" ")
+
+if(mode == "e"):
+    encrypt()
+
+if(mode == "d"):
+    decrypt()
